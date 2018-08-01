4a6b16ee707796e7901ef1e118ec00fcd529d761
SmolFox <cosmicnight.thunderclan@gmail.com>
2018-08-01 08:14:44 -0600

    started to build out application

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
-
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import Picker from './datePicker';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Birthday Countdown</h1>
+      <div className="grid">
+        <div className="title-box">
+          Birthday Countdown
+        </div>
+
+        <div className="skew-light"></div>
+        <div className="skew-light-base"></div>
+        <div className="skew-light-two"></div>
+
+        <div className="skew-dark-base"></div>
+        <div className="skew-dark"></div>
+        <div className="skew-dark-two"></div>
+         <Picker/>
       </div>
     );
   }
